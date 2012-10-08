5f33d965f300a47caabc4c7f3d7e23fac6f5fcdf
Haynes Mathew George <hgeorge@codeaurora.org>
2012-10-08 15:57:25 -0700

    libstagefright: Add support for vendor specific color format
    - Add OMX_QCOM_COLOR_FormatYUV420PackedSemiPlanar32m as a supported color format for ACodec.
    - This format is NV12 + 32 aligned stride and slice (vanilla NV12 is 16 aligned).
    
    Change-Id: I5ec3b06b3eff8396b04aba62114b45e0eab1068d
    
    Conflicts:
    	media/libstagefright/ACodec.cpp

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -1472,6 +1472,7 @@ status_t ACodec::setSupportedOutputFormat() {
            || format.eColorFormat == OMX_TI_COLOR_FormatYUV420PackedSemiPlanar
            || format.eColorFormat == OMX_QCOM_COLOR_FormatYVU420SemiPlanar
 #ifdef QCOM_HARDWARE
+           || format.eColorFormat == OMX_QCOM_COLOR_FormatYUV420PackedSemiPlanar32m
            || format.eColorFormat == OMX_QCOM_COLOR_FormatYVU420PackedSemiPlanar32m4ka
            || format.eColorFormat == OMX_QCOM_COLOR_FormatYUV420PackedSemiPlanar64x32Tile2m8ka
 #endif
